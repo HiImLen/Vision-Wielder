a0721c8aaaf4b0b7d5ec4696f1a033cb8bd58ddc
nmhieu0504 <84891428+nmhieu0504@users.noreply.github.com>
2022-12-30 16:15:48 +0700

    xiangling ultimate

---
+++ Assets/Scripts/Projectile/XianglingUltiProjectileScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XianglingUltiProjectileScript : MonoBehaviour
+{
+    [SerializeField] public float speed = 200f;
+    [SerializeField] private float lifeTime = 12.0f;
+    [SerializeField] private float radius = 1f;
+
+    private float angularSpeed, rotationRadius;
+    private PlayerBehavior playerBehavior;
+    private Rigidbody2D rigidbody2d;
+    private Transform rotationCenter;
+
+    private float posX, posY, angle;
+
+    bool dicrectionUp;
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        playerBehavior = GameObject.FindWithTag("Player").GetComponent<PlayerBehavior>();
+        rotationCenter = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        angularSpeed = 3.0f;
+        rotationRadius = 3.0f;
+        posX = posY = angle = 0f;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        Launch();
+    }
+
+    private void Launch()
+    {
+        // Burst Movement
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector2(posX, posY);
+        angle = angle + Time.deltaTime * angularSpeed;
+        // Debug.Log("posX: " + posX + " posY: " + posY);
+        // Debug.Log("angle: " + angle);
+        // Debug.Log("angularSpeed: " + angularSpeed);
+
+        if (angle >= 360f)
+            angle = 0f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            // Debug.Log("Gouba Hit Enemy!");
+            //other.gameObject.GetComponent<Rigidbody2D>().AddForce((other.gameObject.transform.position - transform.position).normalized * 0.05f);
+            StartCoroutine(other.gameObject.GetComponent<EnemyBehavior>().Damaged(playerBehavior.damage, null));
+        }
+    }
+}
